811ab6db09e541f9ed935987e608706fe360b97a
AlexeyAbramov <abramov.alexey.93@ya.ru>
2019-09-28 16:58:01 +0300

    Updates

---
+++ precache-manifest.4d1942473e32962b167d4226a4dbdf31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ff051b345022cd68fb8f3ceed1adc4",
+    "url": "/aviasales-test-task/index.html"
+  },
+  {
+    "revision": "1a8ea555957e0bdfc842",
+    "url": "/aviasales-test-task/static/css/main.a134421d.chunk.css"
+  },
+  {
+    "revision": "5aeab8cc6e4c86ef6f01",
+    "url": "/aviasales-test-task/static/js/2.ecc939ca.chunk.js"
+  },
+  {
+    "revision": "1a8ea555957e0bdfc842",
+    "url": "/aviasales-test-task/static/js/main.4edcc553.chunk.js"
+  },
+  {
+    "revision": "36f93fbda2a18b67cbf2",
+    "url": "/aviasales-test-task/static/js/runtime-main.14918ba9.js"
+  },
+  {
+    "revision": "c5fcf8456b31edcf9d2d565266b78b2d",
+    "url": "/aviasales-test-task/static/media/Logo.c5fcf845.png"
+  }
+]);
